ab1090c888a3f5d1f1305490a0ca1c576d3fae19
bahama365 <31001392+bahama365@users.noreply.github.com>
2019-04-10 22:04:19 +0100

    Update sslscanScript.sh

--- sslscanScript.sh
+++ sslscanScript.sh
@@ -5,7 +5,7 @@ for hostname in $(cat hostnameList.txt)
 do
         echo "Testing hostname: $hostname" >> outputFile.txt
         echo >> outputFile.txt
-        docker run --rm -t milobahg/sslscan --no-renegotiation --no-compression --no-fallback --no-hearbleed --no-color --tlsall $hostname | grep TLSv1 >> outputFile.txt
+        docker run --rm -t milobahg/sslscan --no-cipher-details --no-renegotiation --no-compression --no-fallback --no-hearbleed --no-colour --tlsall $hostname | grep TLSv1 >> outputFile.txt
         echo >> outputFile.txt
         echo >> outputFile.txt
 done
